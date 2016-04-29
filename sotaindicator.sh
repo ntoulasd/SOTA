f32e46ef76ce52a362debd4717cbea34d753b5b7
Dimitrios Ntoulas <ntoulasd@yahoo.gr>
2016-04-29 23:49:20 +0300

    Create sotaindicator.sh

---
+++ sotaindicator.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Sota notification Running Ctrl+c to stop"  
+while [ 1 ]; do           
+txt=`curl -s http://old.sota.org.uk/RssFeed | grep title | grep -o '>[^<]*<' | grep -o '[^<>]*' | egrep '(SY|SV)[0-9]'`
+
+if [ -n "$txt" ] 
+then
+notify-send SOTA "$txt" --icon=notification-network-wireless-connected
+fi
+sleep 60
+done
