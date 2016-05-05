5a67464aeeda7bdff3e0602d27b40459d838994d
Dimitrios Ntoulas <ntoulasd@yahoo.gr>
2016-05-05 22:55:52 +0300

    Some filtering

--- sotaindicator.sh
+++ sotaindicator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Sota notification Running Ctrl+c to stop"  
 while [ 1 ]; do           
-txt=`curl -s http://old.sota.org.uk/RssFeed | grep title | grep -o '>[^<]*<' | grep -o '[^<>]*' | egrep '(SY|SV)[0-9]'`
+txt=`curl -s http://old.sota.org.uk/RssFeed | grep title | grep -o '>[^<]*<' | grep -o '[^<>]*' | uniq | head -n 4 | egrep '(SY|SV)[0-9]'`
 
 if [ -n "$txt" ] 
 then
